db5c5542daa3f5cac70f99acdd028a489802d5a0
Kingsley Asifor <kingsleyasifor@gmail.com>
2020-12-08 18:35:00 +0100

    change

--- index.php
+++ index.php
@@ -63,3 +63,5 @@ if ($body === false && json_last_error() !== JSON_ERROR_NONE) {
 
 $id = $body->item->id; // (int) 17022701
 $name = $body->item->name; // (string) "SEO Studio - Professional Tools for SEO"
+
+?>
